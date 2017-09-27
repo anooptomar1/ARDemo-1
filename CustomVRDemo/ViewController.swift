d312fb676fe4069cec85b9d8b4e16b62241c6586
zhangzheng <lovezhang.zheng@gmail.com>
2017-09-27 16:23:50 +0800

    Initial Commit

---
+++ CustomVRDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomVRDemo
+//
+//  Created by zhangzheng on 2017/9/27星期三.
+//  Copyright © 2017年 zhangzheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
